a322f5e23cd04b367217aede360df761167cb83a
Ricardo Costa Alves dos Santos <ricardo_cas4@hotmail.com>
2022-04-07 15:40:51 -0300

    :fire: root request working on download a video by query url request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const ytdl = require('ytdl-core');
 
 require('dotenv').config();
 
@@ -16,9 +17,40 @@ app.use(express.json());
 app.use(morgan(morganEnv));
 
 app.get('/', (req, res) => {
+    const { url } = req.query;
+    res.header('Content-Disposition', 'attachmentt; filename="video_baixado.mp4"');
+    return ytdl(url).pipe(res);
+});
+
+app.get('/video', (req, res) => {
     res.json({
         message: 'hello world from main route',
         statusCode: res.statusCode,
+    });
+});
+
+app.get('api/video/info', async (req, res, error, next) => {
+    let { url } = req.query;
+    const requested_url = req.query.url;
+    const videoID = requested_url.split('v=')[1]; // retirando o ID do video Requisitado
+    console.log('requested_ur:l\n', requested_url);
+    console.log('videoID:\n', videoID);
+    res.json({
+        url: requested_url,
+        videoID: videoID,
+    });
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🦄' : error.stack,
     });
 });
 
